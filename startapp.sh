d8098279010f52f9d15ad3cff2443be784c311c8
Björn Grüning <bjoern@gruenings.eu>
2025-08-09 19:08:45 +0200

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -f
 
-PATH=${PATH}:/opt/conda/envs/cellpose/bin
+# Cellpose uses a default starting directory for its internal file browser,
+# which is normally the directory where Cellpose was launched.
+# In Galaxy, $PWD is always a unique job working directory.
+# Here, we override this to provide a more user-friendly default location,
+# making it easier for users to open and save files.
+cd "${CELLPOSE_START_DIR:-$PWD}"
 
-export PATH
-
+export PATH=${PATH}:/opt/conda/envs/cellpose/bin
 cellpose
